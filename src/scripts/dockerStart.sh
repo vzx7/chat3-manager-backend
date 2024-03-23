2fc3fe9f6f6d035b78f0cbbf16eb59ec821c0fd3
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2024-03-23 18:48:04 +0300

    edit scripts

--- src/scripts/dockerStart.sh
+++ src/scripts/dockerStart.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/sh
-# docker start
-# start from ../docker-compose.yml
 systemctl start docker.service
-cd ../docker
 docker-compose up
 exit 0
--- src/scripts/dockerStop.sh
+++ src/scripts/dockerStop.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/sh
-# docker stop
-# stop from ../docker-compose.yml
 
-cd ../docker
 docker-compose down
 exit 0
